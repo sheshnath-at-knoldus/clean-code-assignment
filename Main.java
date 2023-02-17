120946e5db8b3ae03de7851ae9211d1d7152c937
sheshu2906 <sheshnath.yadav@knoldus.com>
2023-02-17 18:02:08 +0530

    modified-2 Main

--- Main.java
+++ Main.java
@@ -4,81 +4,77 @@ import jav.lang.*;
 
 public class Main
 {
-    /* merge algorithm is used to sort the array
-    merging two array in this function leftarray and rightarray
+    /* mergesort algorithm is used to sort the array
     */
-
-    void merge(int array[], int beg, int mid, int end)    
-	{    
-		int index_left_array, index_right_array, k;  
-		int length_left_array = mid - beg + 1;    
-		int length_right_array = end - mid;    
-		  
-	   /* temporary Arrays */  
-			int leftarray[] = new int[length_left_array];  
-			int rightarray[] = new int[length_right_array];  
-		  
-		/* copy data to temp arrays */  
-		for (index_left_array = 0; index_left_array < length_left_array; index_left_array++)    
-		    leftarray[index_left_array] = array[beg + index_left_array]; 
-
-    		for (index_right_array = 0; index_right_array < length_right_array; index_right_array++)    
-		    rightarray[index_right_array] = array[mid + 1 + index_right_array];    
-		  
-
-		int index_of_left_subarry = 0; /* initial index of first sub-array */  
-		int index_of_right_subarray= 0; /* initial index of second sub-array */   
-		int index_of_merged_subarray = beg;  /* initial index of merged sub-array */  
-		  
-
-		while (index_of_left_subarry < length_left_array && index_of_right_subarray < length_right_array)    
-		{    	if(leftarray[index_of_left_subarry] <= rightarray[index_of_right_subarray])    
-			{  
-				array[index_of_merged_subarray] = leftarray[index_of_left_subarry];  
-                  
-				index_of_left_subarry++;    
-			}    
-			else    
-			{    
-				array[index_of_merged_subarray] = rightarray[index_of_right_subarray];    
-				index_of_right_subarray++;    
-			}    
-			index_of_merged_subarray++;    
-		}   
-        
-        
-		while (index_of_left_subarry<length_left_array)    
-		{   array[index_of_merged_subarray] = leftarray[index_of_left_subarry];    
-			index_of_left_subarry ++;    
-			index_of_merged_subarray ++;    
-		}    
-		  
-		while (index_of_right_subarray < length_right_array)    
+	
+	    //merging two array in this function leftarray and rightarray
+		void merge(int array[], int beg, int mid, int end)    
 		{    
-			array[index_of_merged_subarray] = rightarray[index_of_right_subarray];    
-			index_of_right_subarray++;    
-			index_of_merged_subarray++;    
+			int index_left_array, index_right_array;  
+			int length_left_array = mid - beg + 1;    
+			int length_right_array = end - mid;    
+			
+		/* temporary Arrays */  
+				int leftarray[] = new int[length_left_array];  
+				int rightarray[] = new int[length_right_array];  
+			
+			/* copy data to temp arrays */  
+			for (index_left_array = 0; index_left_array < length_left_array; index_left_array++)    
+				leftarray[index_left_array] = array[beg + index_left_array]; 
+
+			for (index_right_array = 0; index_right_array < length_right_array; index_right_array++)    
+				rightarray[index_right_array] = array[mid + 1 + index_right_array];    
+			
+			int index_of_left_subarry = 0; /* initial index of first sub-array */  
+			int index_of_right_subarray= 0; /* initial index of second sub-array */   
+			int index_of_merged_subarray = beg;  /* initial index of merged sub-array */  
+			
+			while (index_of_left_subarry < length_left_array && index_of_right_subarray < length_right_array)    
+			{    	if(leftarray[index_of_left_subarry] <= rightarray[index_of_right_subarray])    
+				{  
+					array[index_of_merged_subarray] = leftarray[index_of_left_subarry];         
+					index_of_left_subarry++;    
+				}    
+				else    
+				{    
+					array[index_of_merged_subarray] = rightarray[index_of_right_subarray];    
+					index_of_right_subarray++;    
+				}    
+				index_of_merged_subarray++;    
+			}   
+			
+			
+			while (index_of_left_subarry<length_left_array)    
+			{   array[index_of_merged_subarray] = leftarray[index_of_left_subarry];    
+				index_of_left_subarry ++;    
+				index_of_merged_subarray ++;    
+			}    
+			
+			while (index_of_right_subarray < length_right_array)    
+			{   array[index_of_merged_subarray] = rightarray[index_of_right_subarray];    
+				index_of_right_subarray++;    
+				index_of_merged_subarray++;    
+			}    
 		}    
-	}    
 	  
 
-    /*Here sorting of array is performed in this function*/
+     /*Here sorting of array is performed in this function*/
 
-	void mergeSort(int array[], int beg, int end)  
-	{  
-		if (beg < end)   
+		void mergeSort(int array[], int beg, int end)  
 		{  
-			int mid = (beg + end) / 2;  
-			mergeSort(array, beg, mid);  
-			mergeSort(array, mid + 1, end);  
-			merge(array, beg, mid, end);  
-		}  
-	}  
+			if (beg < end)   
+			{  
+				int mid = (beg + end) / 2;  
+				mergeSort(array, beg, mid);  
+				mergeSort(array, mid + 1, end);  
+				merge(array, beg, mid, end);  
+			}  
+		}   
+	      
+		/*main method */
 
-
-            /*main method */
-
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
         //creating input variable  using scanner class
 		
@@ -91,19 +87,14 @@ public class Main
 		
         // array creation 
         int first_array[] =new int[size_of_array];
+        int second_array[] = new int[size_of_array];	
         
-		int second_array[] = new int[size_of_array];	
-        
-		
-
-        //array input
+		//array input
 
 		for(int i=0;i<size_of_array;i++)
 		{
 		    first_array[i]=input.nextInt();
 		}
-
-
 
 		// coping array from first to second array
 
@@ -115,18 +106,14 @@ public class Main
 		//calling mergesort method  
         object.mergeSort(second_array, 0, size_of_array - 1);
 		
-		
-		
-		
 		System.out.println();
 		System.out.print("[");
 
         /*comparing two arrays and printing the indexes according to the sorted array */
-		
-		//iterating through first array
+			
 		for(int i=0;i<size_of_array;i++)
 	    {
-			//iterarting through second array
+			
 		   for(int j=0;j<size_of_array;j++)
 		   {
 	
